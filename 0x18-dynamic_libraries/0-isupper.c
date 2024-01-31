bd0cd89963731cb0ee354f4900419e2807b7b4d5
aumizy <aumizy01@gmail.com>
2024-01-30 17:05:04 -0800

    added 0-2 files

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,16 @@
+/**
+* Description:  a function that checks for uppercase
+* character.
+* Return: 0 (success)
+*/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
